d7806ad97815913dbb188b02fbeb4a79cd0deed2
Pawel Kowalski <pavelloz@gmail.com>
2017-12-20 11:24:48 +0000

    Add npm deps. installation

--- generators/blank-marketplace/index.js
+++ generators/blank-marketplace/index.js
@@ -1,9 +1,6 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const path = require("path");
-const mkdirp = require('mkdirp');
-
-
 
 module.exports = class extends Generator {
   prompting() {
@@ -36,51 +33,40 @@ module.exports = class extends Generator {
   }
 
   writing() {
-    const projectName = this.props.name.replace(/\s+/g, '-').toLowerCase();
-    const projectDir = `marketplace-${projectName}`;
+    const projectName = this.props.name.replace(/\s+/g, "-").toLowerCase();
+    this.projectDir = `marketplace-${projectName}`;
 
-    console.log("Create marketplace files in ", projectDir)
-    mkdirp.sync(projectDir);
-
-    this.fs.copy(
-      this.templatePath('files/**'),
-      this.destinationPath(projectDir),
-      {
-        globOptions: { dot: true }
-      }
-    );
-
+    this.fs.copy(this.templatePath("files/**"), this.destinationPath(this.projectDir), {
+      globOptions: { dot: true }
+    });
 
     this.fs.copyTpl(
       this.templatePath("files/marketplace_builder/.builder"),
-      this.destinationPath(projectDir + "/marketplace_builder/.builder" ),
+      this.destinationPath(this.projectDir + "/marketplace_builder/.builder"),
       {
         PRODUCTION_URL: this.props.production,
         STAGING_URL: this.props.staging,
-        LOCAL_URL: this.props.local,
+        LOCAL_URL: this.props.local
       }
     );
 
-    this.fs.copyTpl(
-      this.templatePath("README.md"),
-      this.destinationPath(projectDir + '/README.md'),
-      {
-        NAME: this.props.name,
-        DIR: projectDir
-      }
-    );
+    this.fs.copyTpl(this.templatePath("README.md"), this.destinationPath(this.projectDir + "/README.md"), {
+      NAME: this.props.name,
+      DIR: this.projectDir
+    });
 
-    this.fs.copyTpl(
-      this.templatePath("package.json"),
-      this.destinationPath(projectDir + '/package.json'),
-      {
-        NAME: this.props.name,
-        PROJECT_NAME: projectName
-      }
-    );
+    this.fs.copyTpl(this.templatePath("package.json"), this.destinationPath(this.projectDir + "/package.json"), {
+      NAME: this.props.name,
+      PROJECT_NAME: projectName
+    });
   }
 
-  install() {}
+  install() {
+    console.log(chalk.green("MPP :: BlankMarketplace :: Installing NPM dependencies"));
+
+    process.chdir(`${process.cwd()}/${this.projectDir}`);
+    this.npmInstall();
+  }
 
   postInstall() {
     console.log(chalk.green("MPP :: BlankMarketplace :: Module files generated"));
